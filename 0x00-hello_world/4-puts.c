97c405f330eac989655c3148cdff66d1ea3425a7
khaled <khaledshazly67@gmail.com>
2023-06-16 10:47:06 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void) { 
-	puts("Programming is like building a multilingual puzzle\n");
-       	return (0);
+/**
+*main - Entry point Description: A C program that prints with put function 
+*Retrrn: alawys 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")return (0);
 }
