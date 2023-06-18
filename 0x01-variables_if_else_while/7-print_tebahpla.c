7fcd67283afbeba20a428dbd590e1ad0e8d816f7
khaled <khaledshazly67@gmail.com>
2023-06-18 10:48:04 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include<stdio.>
+
+/**
+ * main - enter point
+ *
+ * Description: print a- iin reversd
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch));
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
