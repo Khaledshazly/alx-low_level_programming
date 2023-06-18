eefcfca716f61df1e09f5e8e86c3a81ff2307e32
khaled <khaledshazly67@gmail.com>
2023-06-18 09:44:47 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * main - entery point
  *
- * Description: 
+ * Description: print all sigle number
  *
  * Return: 0 (success)
 */
 
 int main(void)
 {
-	int digit =0;
+	int digit = 0;
 	while (digit <= 9)
 	{
 		printf("%i" . digit);
